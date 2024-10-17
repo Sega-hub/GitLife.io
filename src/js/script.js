1ec51087253ac5483e5a187d6c53fbc8c86de6b0
Sergey Semenyuk <serdjefre@gmail.com>
2024-10-17 16:59:35 +0300

    fix animate scroll bag

--- src/js/script.js
+++ src/js/script.js
@@ -5,12 +5,13 @@ cards.forEach(card => {
     card.addEventListener("mousemove", (e) => {
         const effectWidth = effect.offsetWidth / 2;
         const effectHeight = effect.offsetHeight / 2;
-        effect.style.opacity = 1;
+        effect.style.display = "flex";
         effect.style.top = (e.pageY - effectHeight) + "px";
         effect.style.left = (e.pageX - effectWidth) + "px";
+        document.body.style.overflowX = "hidden";
          
     });
     card.addEventListener("mouseleave", () => {
-        effect.style.opacity = 0;
+        effect.style.display = "none";
     });
 });
